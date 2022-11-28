e5c0d03389baa25e15115b7799dd605fd6334beb
Barbara Castilho <barbara.castilho@outlook.com.br>
2022-11-28 15:27:53 +0000

    Funcional commit

--- src/index.js
+++ src/index.js
@@ -1,57 +1,87 @@
 /** @jsx h */
 
 function h(type, props, ...children) {
-  return { type, props, children };
+    return { type, props, children };
 }
 
 function createElement(node) {
-  if (typeof node === "string") {
-    return document.createTextNode(node);
-  }
-  const $el = document.createElement(node.type);
-  node.children.map(createElement).forEach($el.appendChild.bind($el));
-  return $el;
+    if (typeof node === "string") {
+        return document.createTextNode(node);
+    }
+    const $el = document.createElement(node.type);
+    node.children.map(createElement).forEach($el.appendChild.bind($el));
+    return $el;
 }
 
-function updateElement() {
-  // TODO: implement
+function changed(node1, node2) {
+    return typeof node1 !== typeof node2 ||
+        typeof node1 === 'string' && node1 !== node2 ||
+        node1.type !== node2.type
+}
+
+function updateElement($parent, newNode, oldNode, index = 0) {
+    if (!oldNode) {
+        $parent.appendChild(
+            createElement(newNode)
+        );
+    } else if (!newNode) {
+        $parent.removeChild(
+            $parent.childNodes[index]
+        );
+    } else if (changed(newNode, oldNode)) {
+        $parent.replaceChild(
+            createElement(newNode),
+            $parent.childNodes[index]
+        );
+    } else if (newNode.type) {
+        const newLength = newNode.children.length;
+        const oldLength = oldNode.children.length;
+        for (let i = 0; i < newLength || i < oldLength; i++) {
+            updateElement(
+                $parent.childNodes[index],
+                newNode.children[i],
+                oldNode.children[i],
+                i
+            );
+        }
+    }
 }
 
 const initDOM = (
-  <div>
-    <p>Hello!</p>
-    <ul>
-      <li>How is it going?</li>
-    </ul>
-  </div>
+    <div>
+        <p>Hello!</p>
+        <ul>
+            <li>How is it going?</li>
+        </ul>
+    </div>
 );
 
 const addNode = (
-  <div>
-    <p>Hello!</p>
-    <ul>
-      <li>How is it going?</li>
-    </ul>
-    <p>Good</p>
-  </div>
+    <div>
+        <p>Hello!</p>
+        <ul>
+            <li>How is it going?</li>
+        </ul>
+        <p>Good</p>
+    </div>
 );
 
 const removeNode = (
-  <div>
-    <p>Hello!</p>
-    <ul>
-      <li>How is it going?</li>
-    </ul>
-  </div>
+    <div>
+        <p>Hello!</p>
+        <ul>
+            <li>How is it going?</li>
+        </ul>
+    </div>
 );
 
 const changeNode = (
-  <div>
-    <p>Hi!</p>
-    <ul>
-      <li>How is it going?</li>
-    </ul>
-  </div>
+    <div>
+        <p>Hi!</p>
+        <ul>
+            <li>How is it going?</li>
+        </ul>
+    </div>
 );
 
 const rootElement = document.getElementById("root");
@@ -61,14 +91,14 @@ const initNodeButton = document.createElement("button");
 initNodeButton.innerText = "Init";
 rootElement.appendChild(initNodeButton);
 initNodeButton.addEventListener("click", () => {
-  updateElement(rootElement, initDOM, initDOM);
+    updateElement(rootElement, initDOM, initDOM);
 });
 
 const addNodeButton = document.createElement("button");
 addNodeButton.innerText = "Add";
 rootElement.appendChild(addNodeButton);
 addNodeButton.addEventListener("click", () => {
-  updateElement(rootElement, addNode, initDOM);
+    updateElement(rootElement, addNode, initDOM);
 });
 
 const removeNodeButton = document.createElement("button");
@@ -76,7 +106,7 @@ removeNodeButton.innerText = "Remove";
 rootElement.appendChild(removeNodeButton);
 
 removeNodeButton.addEventListener("click", () => {
-  updateElement(rootElement, removeNode, addNode);
+    updateElement(rootElement, removeNode, addNode);
 });
 
 const changeNodeButton = document.createElement("button");
@@ -84,5 +114,5 @@ changeNodeButton.innerText = "Change";
 rootElement.appendChild(changeNodeButton);
 
 changeNodeButton.addEventListener("click", () => {
-  updateElement(rootElement, changeNode, removeNode);
+    updateElement(rootElement, changeNode, removeNode);
 });
